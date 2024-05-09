e1235a5491746a6a6d1145d3efc6f9f4c1ff4a0c
akul-saxena <akul.saxenaa@gmail.com>
2024-05-09 14:45:21 +0530

    Best Practices.

--- php/add_todo.php
+++ php/add_todo.php
@@ -1,40 +1,41 @@
 <?php
-include 'db_conn.php';
+
+include '../db_conn.php';
 
 class Add
 {
-  private $conn;
+    private $conn;
 
-  public function __construct($conn)
-  {
-    $this->conn = $conn;
-  }
-
-  /**
-   * Adds a new todo.
-   *
-   * @param string $todoText It is the text of the todo which will be added.
-   *
-   * @return string Returns whether operation was success or not
-   */
-  public function addTodo($todoText)
-  {
-    // Insert Query
-    $sql = "INSERT INTO todos (todo_text) VALUES ('$todoText')";
-
-    if ($this->conn->query($sql) === TRUE) {
-      return "Todo added successfully";
-    } else {
-      return "Error: " . $sql . "<br>" . $this->conn->error;
+    public function __construct($conn)
+    {
+        $this->conn = $conn;
     }
-  }
+
+    /**
+     * Adds a new todo.
+     *
+     * @param string $todoText It is the text of the todo which will be added.
+     *
+     * @return string Returns whether operation was success or not
+     */
+    public function addTodo($todoText)
+    {
+        // Insert Query
+        $sql = "INSERT INTO todos (todo_text) VALUES ('$todoText')";
+
+        if ($this->conn->query($sql) === true) {
+            return "Todo added successfully";
+        } else {
+            return "Error: " . $sql . "<br>" . $this->conn->error;
+        }
+    }
 }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $todoText = $_POST['todo_text'];
+    $todoText = $_POST['todo_text'];
 
-  $Add = new Add($conn);
-  echo $Add->addTodo($todoText);
+    $Add = new Add($conn);
+    echo $Add->addTodo($todoText);
 }
 
 $conn->close();
--- php/get_todos.php
+++ php/get_todos.php
@@ -1,47 +1,48 @@
 <?php
-include 'db_conn.php';
+
+include '../db_conn.php';
 
 class Get
 {
-  private $conn;
+    private $conn;
 
-  public function __construct($conn)
-  {
-    $this->conn = $conn;
-  }
-
-  /**
-   * Gets all todos.
-   *
-   * @return array Returns an array of todos.
-   */
-  public function getTodos()
-  {
-    // Select Query
-    $sql = "SELECT * FROM todos";
-    $result = $this->conn->query($sql);
-    $todos = [];
-
-    if ($result->num_rows > 0) {
-      while ($row = $result->fetch_assoc()) {
-        $todos[] = $row;
-      }
+    public function __construct($conn)
+    {
+        $this->conn = $conn;
     }
 
-    return $todos;
-  }
+    /**
+     * Gets all todos.
+     *
+     * @return array Returns an array of todos.
+     */
+    public function getTodos()
+    {
+        // Select Query
+        $sql = "SELECT * FROM todos";
+        $result = $this->conn->query($sql);
+        $todos = [];
+
+        if ($result->num_rows > 0) {
+            while ($row = $result->fetch_assoc()) {
+                $todos[] = $row;
+            }
+        }
+
+        return $todos;
+    }
 }
 
 $Get = new Get($conn);
 $todos = $Get->getTodos();
 
 if (!empty($todos)) {
-  //If a todo item is present, print the card with item and edit, delete and mark done buttons
-  foreach ($todos as $todo) {
-    $todoId = $todo["id"];
-    $todoText = $todo["todo_text"];
-    $status = $todo["status"];
-    echo "<div class='todo-item " . ($status == 'done' ? 'done' : '') . "' data-id='$todoId'>
+    //If a todo item is present, print the card with item and edit, delete and mark done buttons
+    foreach ($todos as $todo) {
+        $todoId = $todo["id"];
+        $todoText = $todo["todo_text"];
+        $status = $todo["status"];
+        echo "<div class='todo-item " . ($status == 'done' ? 'done' : '') . "' data-id='$todoId'>
                 <p class='todo-text'>$todoText </p>
                   <input type='text' class='edit-input' value='$todoText''>
                   <div>
@@ -50,9 +51,9 @@ if (!empty($todos)) {
                   <button class='delete-todo' data-id='$todoId'>Delete</button>
                   </div>
             </div>";
-  }
+    }
 } else {
-  echo "Add an Item!";
+    echo "Add an Item!";
 }
 
 $conn->close();
